27e1f5f7d51095c45c591f5f5aa1fd171aa565e9
Tushar Vinayak <tusharvinayak6@gmail.com>
2021-12-29 23:33:27 +0530

    Add files via upload

---
+++ main/main.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import pyautogui as pag
+
+
+# In[2]:
+
+
+def login():
+    username = driver.find_element_by_id("login-email")
+    username.send_keys("username")
+    password = driver.find_element_by_id("login-password")
+    password.sed_keys("password")
+    driver.find_element_by_id("login-submit").click()
+
+
+# In[3]:
+
+
+def goto_network():
+    driver.find_element_by_id("mynetwork-tab-icon").click()
+
+
+# In[4]:
+
+
+def send_requests():
+    n = input("Number of requests : ")
+    for i in range(0,n):
+        pag.click(880, 770)
+    print("Done!!!")    
+
+
+# In[5]:
+
+
+def main():
+    url = "http://linkedin.com/"
+    network_url = "http://linkedin.com/mynetwork/"
+    driver = webdriver.Chrome('./chromedriver.exe')
+    driver.get(url)
+
+
+# In[7]:
+
+
+if __name__ == "__main__":
+    main()
+
+
+# In[ ]:
+
+
+
+
